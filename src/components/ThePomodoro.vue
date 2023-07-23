e9e5da4e969970950df208f41afcc82348f72584
Towkir Ahmed <towkir17@gmail.com>
2023-07-24 00:18:57 +0600

    added sass and removed template files

---
+++ src/components/ThePomodoro.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
