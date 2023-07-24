6cf8b33cafd1ae69207d20ed195b41c5d09e1b19
Towkir Ahmed <towkir17@gmail.com>
2023-07-24 22:26:34 +0600

    added style files and imported fonts

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig((mode) => {
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "./src/assets/styles/variables.scss";
+            @import "./src/assets/styles/mixins.scss";
+          `,
+        },
+      },
+      devSourcemap: mode === 'development',
+    },
   }
 })
